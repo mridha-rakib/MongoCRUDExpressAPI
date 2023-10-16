b4d66101fc6c522d37690042bd8d900af3108a21
RAKIB <rakibmahmudmridha@gmail.com>
2023-10-16 11:44:44 +0600

    routHandler add

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const todoHandler = require("./routeHandler/todoHandler");
+
+// express app initialization
+const app = express();
+app.use(express.json());
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/todos")
@@ -13,6 +18,9 @@ mongoose
     console.log(err);
     process.exit(1);
   });
+
+// application routes
+app.use("/todo", todoHandler);
 
 app.listen(3000, () => {
   console.log("Listening port on : 3000");
